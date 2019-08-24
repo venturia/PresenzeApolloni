5cda7a77313e53d69a0069e016f1b4872379c4e0
Andrea Venturi <andrea.venturi@cern.ch>
2019-08-24 11:23:48 +0200

    Modificato range IP interni ed eliminato controllo con ping

--- script/rivelaMACAddress.sh
+++ script/rivelaMACAddress.sh
@@ -2,11 +2,11 @@
 echo " ---" `date +"%Y-%m-%d %T"`
 for i in {10..50}
 do
-  eval  ping -q -c 1  "192.168.1.$i" >> /dev/null
-  ret_code=$?
-  if [ $ret_code == 0 ]; then
-    /usr/sbin/arp -a "192.168.1.$i" | awk '{if ($4~/:/) print $4}'
-  fi
+#  eval  ping -q -c 1  "192.168.0.$i" >> /dev/null
+#  ret_code=$?
+#  if [ $ret_code == 0 ]; then
+    /usr/sbin/arp -a "192.168.0.$i" | awk '{if ($4~/:/) print $4}'
+#  fi
   
 done
 
